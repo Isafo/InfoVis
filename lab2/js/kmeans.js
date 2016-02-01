6d65e876b78c7ab0487e04cde08487a364eb14ad
isafo <isabelle.forsman@gmail.com>
2016-02-01 10:22:05 +0100

    generating k random points, untested code

--- lab2/js/kmeans.js
+++ lab2/js/kmeans.js
@@ -8,7 +8,38 @@
     function kmeans(data, k) {
         
         //...
-        
+        // Randomly place K points into the space
+	if(!k) {
+	    k = 3;
+	}
+
+	var range[];
+	
+
+	for (var dimension in data) {
+	    var extremes[];
+	    
+	    extremes[0] = d3.max(data, function(d){return d[dimension]});
+	    extremes[1] = d3.min(data, function(d){return d[dimension]});
+
+	    range.push(extremes);
+	}
+	
+	var kPoints[];
+	
+	while(k--){
+
+	    var point_pos[];
+	    
+	    // place k random points within the dimension
+	    for(var dimension in data) {
+		point_pos = range[dimension][1] +
+		    (Math.random() * (range[dimension][0] - range[dimension][1]));
+		
+	    }
+	    kPoints.push(point_pos);
+	}
+
+	
     };
-    
-    
+
