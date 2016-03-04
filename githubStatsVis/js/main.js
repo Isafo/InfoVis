34fa5dab41e71cd6f2c18000ab412374151ff894
Gabriella Rydenfors <gabry944@student.liu.se>
2016-03-04 11:15:12 +0100

    reloding page still shows the selected language

--- githubStatsVis/js/main.js
+++ githubStatsVis/js/main.js
@@ -6,4 +6,4 @@ var map = new map();
 d3.select('#title').append('label').text("Top 10 languages in all countries");
 d3.select('#description').append('label').append("small").text("measured by number of repositories in in that languages \n \n");
 
-map.setMode("All");
+map.setMode(document.getElementById("language_select").value);
